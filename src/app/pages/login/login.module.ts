89ad791c3f7dfecb9dab4386f26209cf17d19a7e
amarianjel <abrm.engineer@gmail.com>
2023-08-25 09:38:41 -0400

    Login Diseño - Problema con password en crear usuario, se cambia la disposicion

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -7,8 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { LoginPageRoutingModule } from './login-routing.module';
 
 import { LoginPage } from './login.page';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     CommonModule,
     FormsModule,
