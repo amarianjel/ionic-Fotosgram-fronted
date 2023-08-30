759aada5e57209eb47a176e80c09e3740d084244
amarianjel <abrm.engineer@gmail.com>
2023-08-30 16:11:47 -0400

    Guard y Validar Token

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,8 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage-angular';
 import { Usuario } from '../interfaces/interfaces';
 import { environment } from 'src/environments/environment';
+import { NavController } from '@ionic/angular';
 
 const URL = environment.url;
 
@@ -14,7 +15,7 @@ export class UsuarioService {
   token: string | any;
   private usuario: Usuario = {};
 
-  constructor( private http: HttpClient, private storage: Storage ) {
+  constructor( private http: HttpClient, private storage: Storage, private navCtrl: NavController ) {
     // README: Funcion que da problemas cuando inicia la app, pero se soluciono con un try catch del storage
     this.storage.create();
   }
@@ -47,24 +48,51 @@ export class UsuarioService {
 
   
   registro( usuario: Usuario ) {
-
-    console.log(usuario)
     return new Promise( resolve => {
+      this.http.post(`${ URL }/user/create`, usuario ).subscribe( async( resp:any ) => {
+  
+        if ( resp['ok'] ) {
+          await this.guardarToken( resp['token'] );
+          resolve(true);
+        } else {
+          this.token = null;
+          this.storage.clear();
+          resolve(false);
+        }
+      });
+    });
+  }
 
-      console.log(resolve)
-      console.log(usuario)
-      // this.http.post(`${ URL }/user/create`, usuario ).subscribe( async( resp:any ) => {
-      //   console.log(resp);
+  async cargarToken() {
+    this.token = await this.storage.get('token') || null; //Para leerlo del storage
+  }
 
-      //   if ( resp['ok'] ) {
-      //     await this.guardarToken( resp['token'] );
-      //     resolve(true);
-      //   } else {
-      //     this.token = null;
-      //     this.storage.clear();
-      //     resolve(false);
-      //   }
-      // });
+  async validaToken(): Promise<boolean>{
+    
+    await this.cargarToken();
+
+    if ( !this.token ) {
+      this.navCtrl.navigateRoot('/login');
+      return Promise.resolve(false);
+    }
+    
+    return new Promise<boolean>( resolve => {
+
+      const headers = new HttpHeaders({
+        'x-token': this.token
+      });
+
+      this.http.get(`${ URL }/user/`, { headers })
+        .subscribe(( resp:any ) => {
+
+          if ( resp['ok'] ) {
+            this.usuario = resp['usuario'];
+            resolve(true);
+          } else {
+            this.navCtrl.navigateRoot('/login');
+            resolve(false);
+          }
+        });
     });
   }
 }
